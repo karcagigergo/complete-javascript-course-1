2ed894bf227fbd9eb19fd646906bb96cb6f5dad0
Gergo Karcagi <karcagigergo@gmail.com>
2021-05-30 19:23:35 -0300

    added page a js to show assignments

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,13 @@
+// JS Fundamentals part one: Lecture 1: Values and variables:
+let country = "Hungary";
+let continent = "Europe";
+let population = 10000000;
+console.log(`I am from ${country}, which is in ${continent}, with the population of ${population} people.`);
+
+// JS Fundamentals part one: Lecture 2: Data Types:
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+// JS Fundamentals part one: Lecture 1: Values and variables:
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Gergő');
+// console.log(23);
+
+// let firstName = "Bob";
+// console.log(firstName);
+// let PI = 3.1415;
+
+
+// JS Fundamentals part one: Lecture 2: Data Types:
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Gergő');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// JS Fundamentals part one: Lecture 3: Data Types: Let, Const and Var:
