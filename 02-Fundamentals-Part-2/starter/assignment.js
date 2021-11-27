fc35620e2c56f9218341a78c5c258ad317bbf83c
Gergo Karcagi <karcagigergo@gmail.com>
2021-11-26 23:05:52 -0300

    excercise

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -44,26 +44,105 @@
 // § To calculate average of 3 values, add them all together and divide by 3
 // § To check if number A is at least double number B, check for A >= 2 * B.
 // Apply this to the team's average scores 😉
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-// Test numbers #1
-const doplhinsAvg1 = calcAverage(44, 23, 71);
-const koalasAvg1 = calcAverage(65, 54, 49);
-console.log(doplhinsAvg1, koalasAvg1);
-// Test numbers #2
-const doplhinsAvg2 = calcAverage(85, 54, 41);
-const koalasAvg2 = calcAverage(23, 34, 27);
-console.log(doplhinsAvg2, koalasAvg2);
+// // Test numbers #1
+// const doplhinsAvg1 = calcAverage(44, 23, 71);
+// const koalasAvg1 = calcAverage(65, 54, 49);
+// console.log(doplhinsAvg1, koalasAvg1);
+// // Test numbers #2
+// const doplhinsAvg2 = calcAverage(85, 54, 41);
+// const koalasAvg2 = calcAverage(23, 34, 27);
+// console.log(doplhinsAvg2, koalasAvg2);
 
-const checkWinner = function (avgDolphins, avgKoalas) {
-  if (avgDolphins >= 2 * avgKoalas) {
-    return `Dolphins win! 🐬 (${avgDolphins} vs. ${avgKoalas})`
-  } else if (avgKoalas >= 2 * avgDolphins) {
-    return `Koalas win! 🐨 (${avgKoalas} vs. ${avgDolphins})`
-  } else {
-    return `No team wins!😢`
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     return `Dolphins win! 🐬 (${avgDolphins} vs. ${avgKoalas})`
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     return `Koalas win! 🐨 (${avgKoalas} vs. ${avgDolphins})`
+//   } else {
+//     return `No team wins!😢`
+//   }
+// }
+
+// console.log(checkWinner(doplhinsAvg1, koalasAvg1))
+// console.log(checkWinner(doplhinsAvg2, koalasAvg2))
+
+// Arrays Challenge 2:
+
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) 😉
+// const calcTip = function (bill) {
+//   if (bill >= 50 && bill <= 300) {
+//     return bill * 0.15;
+//   } else {
+//     return bill * 0.2;
+//   }
+// }
+// console.log(calcTip(100));
+
+// const bills = [125, 555, 44]
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips);
+
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+// console.log(total)
+
+//Objects challenge:
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass
+// / (height * height) (mass in kg and height in meter)
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// method on both objects). Store the BMI value to a property, and also return it
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// tall.
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2
+    return this.BMI
   }
 }
 
-console.log(checkWinner(doplhinsAvg1, koalasAvg1))
-console.log(checkWinner(doplhinsAvg2, koalasAvg2))
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2
+    return this.BMI
+  }
+}
+console.log(john.calcBMI())
+console.log(mark.calcBMI())
+
+if (john.calcBMI() > mark.calcBMI()) {
+  console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})!`)
+} else {
+  console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})!`)
+}
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -30,15 +30,87 @@
 
 //FUNCTIONS - LESSON 2
 //DECLARATION
-function calcAge1(birthYear) {
-  return 2021 - birthYear;
-}
-const age1 = calcAge1(1991);
+// function calcAge1(birthYear) {
+//   return 2021 - birthYear;
+// }
+// const age1 = calcAge1(1991);
 
-// FUNCTION EXPRESSION
-const calcAge2 = function (birthYear) {
-  return 2021 - birthYear;
-}
-const age2 = calcAge2(1991);
+// // FUNCTION EXPRESSION
+// const calcAge2 = function (birthYear) {
+//   return 2021 - birthYear;
+// }
+// const age2 = calcAge2(1991);
 
-console.log(age1, age2);
+// console.log(age1, age2);
+
+//Object methods
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtman',
+//   age: 2021 - 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 'Peter', 'Steven']
+// }
+
+// console.log(jonas)
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtman',
+//   birthYear: 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 'Peter', 'Steven'],
+//   hasDriversLicense: true,
+//   // calcAge: function () {
+//   //   console.log(this);
+//   //   return 2021 - this.birthYear
+//   // }
+
+//   calcAge: function () {
+//     this.age = 2021 - this.birthYear
+//     return this.age
+//   },
+//   getSummary: function () {
+//     return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he ${this.hasDriversLicense ? "has a drivers license" : "he doesn't have a drivers license"}.`
+//   }
+// }
+
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+// console.log(jonas.getSummary());
+
+//Loops
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`)
+// }
+
+const jonasArray = [
+  'Jonas',
+  'Schmedtman',
+  2021 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+]
+
+const types = []
+
+for (let i = 0; i < jonasArray.length; i++) {
+  //reading from jonasArray
+  console.log(jonasArray[i])
+  //filling up types array
+  types[i] = typeof jonasArray[i]
+  types.push(typeof jonasArray[i])
+}
+
+console.log(types)
+
+const years = [1991, 2007, 1967, 1667]
+const ages = []
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2021 - years[i])
+}
+console.log(ages)
