0ec48f12b87985349f5675403be6dfe225ec3417
Gergo Karcagi <karcagigergo@gmail.com>
2021-12-02 23:07:28 -0300

    Reverting to commit: 637f6ad

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,11 +1,3 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
 
-const x = 23;
-
-if (x === 23) console.log(23);
-
-const calcAge = (birthYear) => 2037 - birthYear;
-
-console.log(calcAge(2001));
-console.log(calcAge(1122));
